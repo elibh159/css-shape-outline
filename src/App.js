c46572aff5eacc0713a357595e7f59dc4e4af471
Elham Baradaran Heravi <51048982+elibh159@users.noreply.github.com>
2022-06-26 12:33:13 +0430

    write test and reproduce nested layout base on agenda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div data-testid="wrapper" className="wrapper">
+          <div className="float">
+            <div data-testid="box" className="box">
+              Text cannot overlap with this box.
+            </div>
+          </div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore 
+          magna aliqua. Ut enim ad minim veniam, quis nostrud 
+          exercitation ullamco laboris nisi ut aliquip ex officia
+          deserunt mollit anim id est laborum.
+        </div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,71 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("elements", () => {
+  it("should have an element with the data-testid='wrapper'" , () => {
+    render(<App/>);
+    const el = screen.getByTestId("wrapper"); 
+    expect(el).not.toBe(null);
+    expect(el).toBeInTheDocument();
+  });
+  it("should have an element with the data-testid='box'" , () => {
+    render(<App/>);
+    const el = screen.getByTestId("box"); 
+    expect(el).not.toBe(null);
+    expect(el).toBeInTheDocument();
+  });
+  it("should have an element with the data-testid='box' nested to data-testid='wrapper'" , () => {
+    render(<App/>);
+    const wrapper = screen.getByTestId("wrapper"); 
+    const box = screen.getByTestId("box"); 
+    expect(wrapper).toContainElement(box)
+    expect(box).not.toContainElement(wrapper)  
+  });
+  it("should have an element with the data-testid='wrapper' have a class='wrapper'" , () => {
+    render(<App/>);
+    const wrapper = screen.getByTestId("wrapper"); 
+    expect(wrapper).toHaveClass('wrapper');  
+  });
+  it("should have an element with the data-testid='box' have a class='box'" , () => {
+    render(<App/>);
+    const box = screen.getByTestId("box"); 
+    expect(box).toHaveClass('box');  
+  });
+});
+
+describe("text contents", () => {
+  it("should contain original text inside the wrapper", () =>{
+    render(<App/>);
+    const el = screen.getByTestId("wrapper"); 
+    const expected = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex officia deserunt mollit anim id est laborum.";
+    expect(el).toHaveTextContent(expected);
+  });
+  it("should contain original text inside the box", () =>{
+    render(<App/>);
+    const el = screen.getByTestId("box"); 
+    const expected = "Text cannot overlap with this box.";
+    expect(el).toHaveTextContent(expected);
+  });
+});
+describe("layout", () => {
+  beforeEach(() => {
+  Element.prototype.getBoundingClientRect = jest.fn(() => {
+        return {
+            width: 250,
+            height: 250, 
+            top:0,
+            left:0
+        }
+    });
+});
+  it("should have roughly 250px width x 250px height on the wrapper", () => {
+    render(<App/>);
+    const wrapper = screen.getByTestId("wrapper"); 
+    const rect= wrapper.getBoundingClientRect();
+    expect(rect.width).toBeGreaterThanOrEqual(250);
+    expect(rect.width).toBeLessThan(260);
+    expect(rect.height).toBeGreaterThanOrEqual(250);
+    expect(rect.height).toBeLessThan(260);
+  });
 });
